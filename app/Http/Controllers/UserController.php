3879da9f3554be3c94196dabbfef1ec0a2676fde
dickyrey <dicky.rey97@gmail.com>
2019-11-29 03:03:54 +0700

    pontianam rest api ver 1.0.0

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Model\User;
+
+class UserController extends Controller
+{
+    //! Menampilkan semua user
+    public function index()
+    {
+        $user =  User::all();
+
+        if (count($user) > 0) { //!mengecek apakah user kosong atau tidak
+            $res['message'] = "Success!";
+            $res['values'] = $user;
+            return response($res);
+        } else {
+            $res['message'] = "Empty!";
+            return response($res);
+        }
+    }
+    //! menampilkan 1 user berdasarkan id
+    public function show($id)
+    {
+        $data = User::where('id', $id)->get();
+
+        if (count($data) > 0) { //mengecek apakah data kosong atau tidak
+            $res['message'] = "Success!";
+            $res['user'] = $data;
+            return response($res);
+        } else {
+            $res['message'] = "Failed!";
+            return response($res);
+        }
+    }
+
+    //! membuat user baru
+    public function create(request $request)
+    {
+        $user = new User;
+        $user->nama = $request->nama;
+        $user->kelurahan = $request->kelurahan;
+        $user->nik = $request->nik;
+        $user->no_kk = $request->no_kk;
+        $user->alamat = $request->alamat;
+        $user->kecamatan = $request->kecamatan;
+        $user->latitude = $request->latitude;
+        $user->longitude = $request->longitude;
+
+        if ($user->save()) {
+            $res['message'] = "Success!";
+            $res['user'] = "$user";
+            return response($res);
+        }
+    }
+
+    public function update(request $request, $id)
+    {
+        $nama = $request->input('nama');
+        $kelurahan = $request->input('kelurahan');
+        $nik = $request->input('nik');
+        $no_kk = $request->input('no_kk');
+        $alamat = $request->input('alamat');
+        $kecamatan = $request->input('kecamatan');
+        $latitude = $request->input('latitude');
+        $longitude = $request->input('longitude');
+
+        $user = User::where('id', $id)->first();
+        $user->nama = $nama;
+        $user->kelurahan = $kelurahan;
+        $user->nik = $nik;
+        $user->no_kk = $no_kk;
+        $user->alamat = $alamat;
+        $user->kecamatan = $kecamatan;
+        $user->latitude = $latitude;
+        $user->longitude = $longitude;
+
+        //!Respons Message
+        if ($user->save()) {
+            $res['message'] = "Success!";
+            $res['value'] = "$user";
+            return response($res);
+        } else {
+            $res['message'] = "Failed!";
+            return response($res);
+        }
+    }
+
+    public function delete($id)
+    {
+        $user = User::find($id);
+
+        if ($user->delete()) {
+            $res['message'] = "Success!";
+            $res['value'] = "$user";
+            return response($res);
+        } else {
+            $res['message'] = "Failed!";
+            return response($res);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('user', 'UserController@index'); //!mengambil semua user
+Route::get('/user/{id}', 'UserController@show'); //!mengambil 1 user berdasarkan id
+Route::post('user', 'UserController@create');
+Route::post('/user/{id}', 'UserController@update');
+Route::delete('/user/{id}', 'UserController@delete');
